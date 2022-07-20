7670dbdfadb792e357b91b551b36c935525ada31
mikprin <miksolo@yandex.ru>
2022-07-20 20:47:16 +0400

    sd

---
+++ sd_card_personal_bitfile/README.md
@@ -0,0 +1,5 @@
+This is code for systemctl module to execute `mv personal/* .` whenever /dev/sda is present.
+
+Path for the file: 
+`/etc/systemd/system/sd_card_check.service`
+chmod 664 to it
---
+++ sd_card_personal_bitfile/sd_check.sh
@@ -0,0 +1,58 @@
+#/bin/bash
+
+cd $HOME
+sd_dir=$HOME/sd
+mkdir -p $sd_dir
+
+
+# DEV=/dev/sda
+# if [ -f "$DEV" ]; then
+#     echo "$DEV exists."
+# else
+#     echo "$DEV not exists."
+# fi
+
+check_mount() {
+    DEV=$1
+    if [ -f "$DEV" ]; then
+        echo "$DEV exists."
+        mount $DEV $sd_dir
+        cd $sd_dir
+        mv personal/* ..
+        cd $HOME
+        sleep 3
+        fusermount -u $sd_dir
+        if $? -eq 0
+        then
+            sleep 7
+            return 0
+        else
+            return 1
+        fi 
+        
+    else
+        echo "$DEV not exists."
+        return 1
+    fi
+}
+
+
+while true
+do
+    DEV=/dev/sda
+
+    check_mount $DEV
+
+    DEV=/dev/sdb
+
+    check_mount $DEV
+
+    DEV=/dev/sdc
+
+    check_mount $DEV
+
+    sleep 1
+done
+
+
+
